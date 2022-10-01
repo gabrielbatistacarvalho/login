adf392cb7acabb16e3fd9ba067a5c40edb045751
gabrielbatistacarvalho <gabriel_carvalho26@hotmail.com>
2022-10-01 13:05:05 -0300

    atualização do módulo de login

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Este projeto tem como objetivo desenvolver um login utilizando as tecnologias do JWT para validação.
+
+https://www.youtube.com/watch?v=qEBoZ8lJR3k&list=PLnDvRpP8BneyHealXbzntUoFtE4SrFWWW&index=2
+
+minuto 42:13 erro no findOne()
--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express()
 // Config JSON response
 app.use(express.json())
 
+// Models
+const User = require('./models/User')
+
 app.get('/', (req, res) => {
     console.log('Estamos conectados!')
     res.status(200).json({ message: 'Estamos conectados!' })
@@ -29,7 +32,49 @@ app.post('/auth/register', async(req, res) => {
     if(!name) {
         return res.status(422).json({ msg: 'O nome é obrigatório!' })
     }
+    // validations
+    if(!email) {
+        return res.status(422).json({ msg: 'O email é obrigatório!' })
+    }
+    // validations
+    if(!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatória!' })
+    }
 
+    if(password != confirmpassword) {    
+        return res.status(422).json({ msg: 'As senhas não conferem!' })
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({ email: email })
+/*
+    if(userExists) {
+        return res.status(422).json({ msg: 'Por favor, utilize outro e-mail!' })
+    }
+
+    // Create password
+    const salt         = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = new User({
+        name,
+        email,
+        password
+    })
+
+    try {
+
+        await user.save()
+
+        res.status(201).json({ msg: 'Usuário criado com sucesso!' })
+        
+    } catch (error) {
+        
+        console.log(error)
+        res.status(500).json({ msg: 'Erro no servidor!' })
+
+    }*/
 })
 
 // Credencials
